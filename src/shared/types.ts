ef69e60b572952ba7614b86685183e4b016a2dee
Huy Bui <huybui@Huys-Mac-mini.local>
2020-12-09 20:27:49 +0200

    Add hard-code dishes data

---
+++ src/shared/types.ts
@@ -0,0 +1,18 @@
+export type Dish = {
+    id: number,
+    name: string,
+    image: string,
+    category: string,
+    label: string,
+    price: string,
+    description: string,
+    comments: Array<Comment>
+}
+
+export type Comment = {
+    id: number,
+    rating: number,
+    comment: string,
+    author: string,
+    date: string
+}
