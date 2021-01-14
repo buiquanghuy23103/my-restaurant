7320cf5bb77f0720922f9283150b0cc76f9c4bc6
buiquanghuy23103 <huythdnbkltv@gmail.com>
2021-01-14 21:07:58 +0200

    Feature: Display dish details on click

--- src/components/DishCard.tsx
+++ src/components/DishCard.tsx
@@ -3,16 +3,19 @@ import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
 import { Dish } from '../shared/types';
 
 type DishCardProps = {
-    dish: Dish
+    dish: Dish,
+    onItemClick: () => void
 }
 
 export default function DishCard(props: DishCardProps) {
+
+
     return (
         <div className="col-12 col-md-5 m-1">
-            <Card tag="li">
-                    <CardImg width="100%" src={ props.dish.image } alt={ props.dish.name } />
+            <Card onClick={ props.onItemClick }>
+                <CardImg width="100%" src={ props.dish.image } alt={ props.dish.name } />
                 <CardImgOverlay>
-                    <CardTitle heading>{ props.dish.name }</CardTitle>
+                    <CardTitle>{ props.dish.name }</CardTitle>
                 </CardImgOverlay>
             </Card>
         </div>
--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Dish } from '../shared/types';
 import DishCard from './DishCard';
+import { Card, CardBody, CardImg, CardText, CardTitle } from 'reactstrap';
+
 
 
 type MenuProps = {
@@ -9,9 +11,33 @@ type MenuProps = {
 
 export default function Menu(props: MenuProps) {
 
+    const [selectedDish, setSelectedDish] = useState<Dish | null>(null);
+
+    function renderDishDetails() {
+        console.log("Function is called");
+
+        if (selectedDish) {
+            return (
+                <Card>
+                    <CardImg width="100%" src={ selectedDish.image } alt={ selectedDish.name } />
+                    <CardBody>
+                        <CardTitle>{ selectedDish.name }</CardTitle>
+                        <CardText>{ selectedDish.description }</CardText>
+                    </CardBody>
+                </Card>
+            );
+        } else {
+            return (<div></div>)
+        }
+    }
+
+
     const menu = props.dishes.map((dish) => {
         return (
-            <DishCard key={ dish.id } dish={ dish } />
+            <DishCard
+                key={ dish.id }
+                dish={ dish }
+                onItemClick={ () => setSelectedDish(dish) } />
         );
     });
 
@@ -19,6 +45,9 @@ export default function Menu(props: MenuProps) {
         <div className="container">
             <div className="row">
                 { menu }
+            </div>
+            <div className="row">
+                { renderDishDetails() }
             </div>
         </div>
     )
