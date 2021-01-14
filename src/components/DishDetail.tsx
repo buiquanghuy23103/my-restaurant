2bf8a26448a0b631d2dda7409272ab56923c1a5c
buiquanghuy23103 <huythdnbkltv@gmail.com>
2021-01-14 22:25:25 +0200

    Feature: show comments on dish detail

--- src/components/DishDetail.tsx
+++ src/components/DishDetail.tsx
@@ -7,13 +7,38 @@ type DishDetailProps = {
 };
 
 export default function DishDetail(props: DishDetailProps) {
+
+    function renderComments() {
+        const comments = props.dish.comments;
+        if (comments.length > 0) {
+            return comments.map(comment => (
+                <div>
+                    <p>{ comment.comment }</p>
+                    <p>---{ comment.author }</p>
+                </div>
+            ));
+        } else {
+            return (<p>No comments</p>)
+        }
+    }
+
     return (
-        <Card>
-            <CardImg width="100%" src={ props.dish.image } alt={ props.dish.name } />
-            <CardBody>
-                <CardTitle>{ props.dish.name }</CardTitle>
-                <CardText>{ props.dish.description }</CardText>
-            </CardBody>
-        </Card>
+        <div className="row">
+            <div className="col-12 col-md-5 m-1">
+                <Card>
+                    <CardImg width="100%" src={ props.dish.image } alt={ props.dish.name } />
+                    <CardBody>
+                        <CardTitle>{ props.dish.name }</CardTitle>
+                        <CardText>{ props.dish.description }</CardText>
+                    </CardBody>
+
+                </Card>
+
+            </div>
+            <div className="col-12 col-md-5 m-1">
+                <h4>Comments</h4>
+                { renderComments() }
+            </div>
+        </div>
     )
 }
