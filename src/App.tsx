757785908e3883448e33895ae15a1a8358caf3e1
buiquanghuy23103 <huythdnbkltv@gmail.com>
2021-01-15 20:39:53 +0200

    Feature: Add basic route by links

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
 import React from 'react';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 import Footer from './components/Footer';
 import Header from './components/Header';
+import Home from './pages/Home';
 import Menu from './pages/Menu';
 
 
 export default function App() {
 
   return (
-    <div>
+    <BrowserRouter>
       <Header />
-      <Menu />
+      <Switch>
+        <Route exact path="/" component={ Home } />
+        <Route exact path="/menu" component={ Menu } />
+        <Redirect to="/" />
+      </Switch>
       <Footer />
-    </div>
+    </BrowserRouter>
   )
 }
