776638d417d0cc6a41c295c0fea57b824b88cd50
buiquanghuy23103 <huythdnbkltv@gmail.com>
2021-01-15 21:08:03 +0200

    UI: Add nav items and collapse buttons for phone

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,65 @@
-import React from 'react'
-import { Jumbotron, Navbar, NavbarBrand } from 'reactstrap'
+import React, { useState } from 'react'
+import { NavLink } from 'react-router-dom'
+import { Collapse, Jumbotron, Nav, Navbar, NavbarBrand, NavbarToggler, NavItem } from 'reactstrap'
 
 export default function Header() {
+
+    const [shouldCollapse, setShouldCollapse] = useState(false);
+
+    function toggleNav() {
+        setShouldCollapse(!shouldCollapse);
+    }
+
     return (
         <React.Fragment>
-            <Navbar dark>
+            <Navbar dark expand="md">
                 <div className="container">
-                    <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+                    <NavbarToggler onClick={ toggleNav }></NavbarToggler>
+                    <NavbarBrand
+                        href="/"
+                        className="mr-auto">
+                        <img
+                            src="../logo.svg"
+                            height="30"
+                            width="41"
+                            alt="Ristorante Con Fusion" />
+                    </NavbarBrand>
+                    <Collapse isOpen={ shouldCollapse } navbar>
+                        <Nav navbar>
+                            <NavItem>
+                                <NavLink
+                                    className="nav-link"
+                                    to="/">
+                                    <span className="fa fa-home fa-lg" />
+                                Home
+                            </NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink
+                                    className="nav-link"
+                                    to="/aboutus">
+                                    <span className="fa fa-info fa-lg" />
+                                About us
+                            </NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink
+                                    className="nav-link"
+                                    to="/menu">
+                                    <span className="fa fa-list fa-lg" />
+                                Menu
+                            </NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink
+                                    className="nav-link"
+                                    to="/contactus">
+                                    <span className="fa fa-list fa-lg" />
+                                Contact us
+                            </NavLink>
+                            </NavItem>
+                        </Nav>
+                    </Collapse>
                 </div>
             </Navbar>
             <Jumbotron>
