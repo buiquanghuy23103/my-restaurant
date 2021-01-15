e7c8f5deeea5f9875585ea3f69a8f763e29047f8
buiquanghuy23103 <huythdnbkltv@gmail.com>
2021-01-15 17:45:01 +0200

    Refactor: remove useEffect in MainPage.tsx

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,17 +1,12 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from '../components/Menu';
 import { DISHES } from '../shared/dishes';
-import { Dish } from '../shared/types';
 
 
 export default function MainPage() {
 
-    const [dishes, setDishes] = useState([] as Dish[]);
-
-    useEffect(() => {
-        setDishes(DISHES);
-    }, []);
+    const [dishes, setDishes] = useState(DISHES);
 
     return (
         <div>
