1c7854fd0193e7e250eb214df15940881c8e5c67
buiquanghuy23103 <huythdnbkltv@gmail.com>
2021-01-15 21:51:38 +0200

    Fix: typo in Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -12,7 +12,7 @@ export default function Footer() {
                             <li><Link to="/">Home</Link></li>
                             <li><Link to="/menu">Menu</Link></li>
                             <li><Link to="/aboutus">About us</Link></li>
-                            <li><Link to="/contact">Contact</Link></li>
+                            <li><Link to="/contactus">Contact</Link></li>
                         </ul>
                     </div>
                     <div className="col-7 col-sm-5">
