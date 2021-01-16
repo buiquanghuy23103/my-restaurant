37746a6b91db9122f355d34587d10a50fb56e103
buiquanghuy23103 <huythdnbkltv@gmail.com>
2021-01-16 08:44:30 +0200

    Feature: Add Contact Us page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 import Footer from './components/Footer';
 import Header from './components/Header';
+import Contact from './pages/Contact';
 import Home from './pages/Home';
 import Menu from './pages/Menu';
 
@@ -14,6 +15,7 @@ export default function App() {
       <Switch>
         <Route exact path="/" component={ Home } />
         <Route exact path="/menu" component={ Menu } />
+        <Route exact path="/contactus" component={ Contact } />
         <Redirect to="/" />
       </Switch>
       <Footer />
