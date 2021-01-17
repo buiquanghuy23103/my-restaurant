fd9b5590054e15e37bc42cbcbd140d11bf19374c
buiquanghuy23103 <huythdnbkltv@gmail.com>
2021-01-17 19:56:59 +0200

    UI: make loading icon aligned

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -2,9 +2,13 @@ import React from 'react'
 
 export default function Loading() {
     return (
-        <div className="col-12">
-            <span className="fa fa-spinner fa-pulse fa-3x fa-fw text-primary"></span>
-            <p>Loading . . .</p>
+        <div className="container">
+            <div className="row">
+                <div className="col-12">
+                    <span className="fa fa-spinner fa-pulse fa-3x fa-fw text-primary"></span>
+                    <p>Loading . . .</p>
+                </div>
+            </div>
         </div>
     )
 }
