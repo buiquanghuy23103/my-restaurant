6021e426e97ea7451beec02de4a369a6011222d3
buiquanghuy23103 <huythdnbkltv@gmail.com>
2021-01-17 20:16:01 +0200

    UI: show loading icon and error message in Menu page

--- src/pages/Menu.tsx
+++ src/pages/Menu.tsx
@@ -4,12 +4,22 @@ import { Breadcrumb, BreadcrumbItem } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { AppState } from '../redux/configureStore';
+import Loading from '../components/Loading';
+import ErrorText from '../components/ErrorText';
 
 export default function Menu() {
 
     const dishes = useSelector((state: AppState) =>
         state.dishState.dishes
     );
+
+    const dishLoading = useSelector((state: AppState) =>
+        state.dishState.isLoading
+    );
+    const dishError = useSelector((state: AppState) =>
+        state.dishState.errorMessage
+    );
+
 
     const menu = dishes.map((dish) => {
 
@@ -20,22 +30,29 @@ export default function Menu() {
         );
     });
 
-
-    return (
-        <div className="container">
-            <div className="row">
-                <Breadcrumb>
-                    <BreadcrumbItem>
-                        <Link to="/">Home</Link>
+    if (dishLoading) {
+        return <Loading />
+    } else if (dishError) {
+        return <ErrorText errorMessage={ dishError } />
+    } else if (dishes) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <Breadcrumb>
+                        <BreadcrumbItem>
+                            <Link to="/">Home</Link>
+                        </BreadcrumbItem>
+                        <BreadcrumbItem active>
+                            Menu
                     </BreadcrumbItem>
-                    <BreadcrumbItem active>
-                        Menu
-                    </BreadcrumbItem>
-                </Breadcrumb>
+                    </Breadcrumb>
+                </div>
+                <div className="row">
+                    { menu }
+                </div>
             </div>
-            <div className="row">
-                { menu }
-            </div>
-        </div>
-    )
+        )
+    } else {
+        return (<div></div>)
+    }
 }
