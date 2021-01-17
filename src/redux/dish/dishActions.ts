1445e5085862a4717eff214d0b924166bc153364
buiquanghuy23103 <huythdnbkltv@gmail.com>
2021-01-17 21:44:28 +0200

    Feature: fetch dishes from JSON db

--- src/redux/dish/dishActions.ts
+++ src/redux/dish/dishActions.ts
@@ -2,13 +2,16 @@ import { DISHES } from "../../shared/dishes";
 import { AppThunk } from "../configureStore"
 import { INIT_DISHES, DishActionTypes, FAIL_DISH, LOAD_DISH } from "./dishActionTypes"
 import { Dish } from "./dishTypes";
+import axios from 'axios';
+import { BASE_URL } from "../../shared/baseUrl";
 
 export const fetchDishes = (): AppThunk => async (dispatch) => {
     dispatch(loadDish(true));
 
-    setTimeout(() => {
-        dispatch(addDishes(DISHES));
-    }, 2000);
+    return axios.get<Dish[]>(BASE_URL + "/dishes")
+        .then(res => dispatch(initDishes(res.data)))
+        .catch(err => console.log(err));
+
 }
 
 export function loadDish(isLoading: boolean): DishActionTypes {
@@ -18,7 +21,7 @@ export function loadDish(isLoading: boolean): DishActionTypes {
     }
 }
 
-export function addDishes(dishes: Dish[]): DishActionTypes {
+export function initDishes(dishes: Dish[]): DishActionTypes {
     return {
         type: INIT_DISHES,
         payload: dishes
