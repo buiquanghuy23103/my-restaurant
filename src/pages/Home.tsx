04d515a8759a69212ec034fd9cfba0683c83175b
buiquanghuy23103 <huythdnbkltv@gmail.com>
2021-01-17 20:13:06 +0200

    UI: show error message in case of network error in Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import ErrorText from '../components/ErrorText';
 import HomeCard from '../components/HomeCard';
 import Loading from '../components/Loading';
 import { AppState } from '../redux/configureStore';
@@ -14,6 +15,10 @@ export default function Home() {
         state.dishState.isLoading
     );
 
+    const dishError = useSelector((state: AppState) =>
+        state.dishState.errorMessage
+    );
+
 
     const leader = useSelector((state: AppState) =>
         state.leaders.find(leader => leader.featured)
@@ -23,34 +28,41 @@ export default function Home() {
         state.promotions.find(promotion => promotion.featured)
     );
 
-    if (dishLoading) return <Loading />
+    if (dishLoading) {
+        return <Loading />
+    } else if (dishError) {
+        return <ErrorText errorMessage={ dishError } />
+    } else if (dish) {
 
-    return (
-        <div className="container">
-            <div className="row align-items-start">
-                <div className="col-12 col-md m-1">
-                    { dish && (<HomeCard
-                        name={ dish.name }
-                        image={ dish.image }
-                        description={ dish.description }
-                    />) }
-                </div>
-                <div className="col-12 col-md m-1">
-                    { promotion && (<HomeCard
-                        name={ promotion.name }
-                        image={ promotion.image }
-                        description={ promotion.description }
-                    />) }
-                </div>
-                <div className="col-12 col-md m-1">
-                    { leader && (<HomeCard
-                        name={ leader.name }
-                        image={ leader.image }
-                        description={ leader.description }
-                        designation={ leader.designation }
-                    />) }
+        return (
+            <div className="container">
+                <div className="row align-items-start">
+                    <div className="col-12 col-md m-1">
+                        { dish && (<HomeCard
+                            name={ dish.name }
+                            image={ dish.image }
+                            description={ dish.description }
+                        />) }
+                    </div>
+                    <div className="col-12 col-md m-1">
+                        { promotion && (<HomeCard
+                            name={ promotion.name }
+                            image={ promotion.image }
+                            description={ promotion.description }
+                        />) }
+                    </div>
+                    <div className="col-12 col-md m-1">
+                        { leader && (<HomeCard
+                            name={ leader.name }
+                            image={ leader.image }
+                            description={ leader.description }
+                            designation={ leader.designation }
+                        />) }
+                    </div>
                 </div>
             </div>
-        </div>
-    )
+        )
+    } else {
+        return (<div></div>)
+    }
 }
